37858a0e9fca04ed8de261e705576810023d245f
Francisco Costa <francisco@fcosta.pt>
2019-03-15 14:04:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def oxford_comma(array)
 
+  last_element = ""
+
+  if array.length == 1
+    array.join()
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_element = ", and #{array.pop}"
+    new_array = array.join(", ")
+    all_elements = new_array.concat(last_element)
+  end
 end
