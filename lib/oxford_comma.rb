a623d1724955149527ff4351a024a1a1f6c1d421
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-26 17:33:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    array[-1] = "and #{array[-1]}"
+    new_array = array.join(", ")
+  else
+    array.join(", ")
+  end
 end
+
