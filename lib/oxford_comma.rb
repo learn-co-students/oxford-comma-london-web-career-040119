40db5f3e4e688e32d8ba1b7a5c942c5b1204d976
Ay Has <ahashim983@yahoo.com>
2019-03-29 00:01:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  len = array.length
+  if array.length == 1
+    return array.join(" ")
+  end
+  array.push("and")
+  array[array.length-1],array[array.length-2] = array[array.length-2],array[array.length-1]
+  a = array.join(", ")
+  if len >= 3
+    return a.reverse.sub(',','').reverse
+  else
+    return a.gsub!(',','')
+  end
 end
+
+#oxford_comma(["a","b"])
